39f32ffad3651a902b8c9b81b56d63902800c471
33Arsenic75 <122697564+33Arsenic75@users.noreply.github.com>
2024-09-01 15:50:17 +0530

    .

--- src/utils 2/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-	site_name: 'AstroPress',
-	title: 'AstroPress | Astro and Tailwind CSS',
-	description: 'Boilerplate built with Astro and Tailwind CSS',
-	author: 'Nicolas',
-	locale_region: 'de-en',
-	locale: 'en'
-};
